2e5128dc0add11326891ae45ca67957f2b767974
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-04-18 11:41:24 +0100

    index.js

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ try {
   let re = new Rectangle({ width: 100, height: 10});
   s = re.getArea();
   console.log(s);
+  let bg = new Bug({ width: 100, height: 10});
+  s = bg.getArea();
+  console.log(s);
 }
 catch (e) {
-  console.log(e);
+  console.log(e.message);
 }
-
-
